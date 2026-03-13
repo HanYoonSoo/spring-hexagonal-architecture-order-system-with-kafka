5f75c596e322622101e9ee384bdb53d44cd454e7
HanYoonSoo <hdbstn3055@gmail.com>
2026-03-13 14:38:28 +0900

    feat: 필요 의존성 및 Configuration 추가

---
+++ adapter/src/main/java/com/hanyoonsoo/ordersystem/adapter/out/persistence/jpa/config/JpaConfig.java
@@ -0,0 +1,13 @@
+package com.hanyoonsoo.ordersystem.adapter.out.persistence.jpa.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaAuditing
+@EntityScan(basePackages = "com.hanyoonsoo.ordersystem.core.domain")
+@EnableJpaRepositories(basePackages = "com.hanyoonsoo.ordersystem.adapter.out.persistence.jpa.repository")
+public class JpaConfig {
+}
